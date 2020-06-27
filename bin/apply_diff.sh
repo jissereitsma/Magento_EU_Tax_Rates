f6f571023ee6e028ee4318b77a38f7ef5e998c4a
Yireo <info@yireo.com>
2020-06-27 12:44:57 +0200

    Fix bins

--- bin/apply_diff.sh
+++ bin/apply_diff.sh
@@ -4,15 +4,17 @@ diffFile=tax_rates_eu_${diffDate}.diff
 targetFile=tax_rates_eu.csv
 backupFile=${targetFile/.csv/_backup_${diffDate}.csv}
 
-scriptPath=$(realpath $0)
+scriptPath=$(readlink -f $0)
 scriptDir=`dirname $scriptPath`
 diffPath=$scriptDir/../diff/$diffFile
 targetPath=$scriptDir/../$targetFile
 
+cd `dirname $scriptDir`
+git pull -q origin master
+
 test -f $diffPath || exit
 test -f $targetPath || (echo "Target $targetPath does not exist" && exit)
 
-cd `dirname $scriptDir`
 cp $diffPath .
 cp $targetFile $backupFile
 git add $backupFile
@@ -21,5 +23,5 @@ git commit -m "Create new backup" $backupFile
 git apply $diffFile
 git commit -m "Applied diff from file ${diffFile}" $targetFile
 
-git push origin master
+git push -q origin master
 rm $diffFile
